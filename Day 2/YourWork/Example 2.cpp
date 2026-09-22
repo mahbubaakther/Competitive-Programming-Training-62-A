1923cd7b79ff6f8ca716f412d864de5d7ac3bb2b
mahbubaakther <mayishamahbuba7@gmail.com>
2026-09-22 14:29:33 +0600

    Add files via upload

---
+++ Day 2/YourWork/Example 2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<string, int> student;
+
+    student.first = "Mahbuba";
+    student.second = 35;
+    cout << "Name: " << student.first << "\n";
+    cout << "ID: " << student.second << "\n";
+    return 0;
+}
+
+
